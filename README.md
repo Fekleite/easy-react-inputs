d64fa6673b59828bc0b6a418878240e02533fd5a
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-05-16 22:24:57 -0300

    chore: add initial configs 🚀

---
+++ README.md
@@ -0,0 +1,9 @@
+# Easy Input Lib
+
+## Techs
+
+- Vite
+- React
+- Tailwind
+- TypeScript
+- Biome
---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div>
+      <h1>Inputs Libs</h1>
+    </div>
+  );
+}
