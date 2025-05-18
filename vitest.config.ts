586dbcfc2ba875c8447b3ff696816f1640600e4a
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-05-18 14:59:19 -0300

    chore: setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "./src/tests/setup.ts",
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
